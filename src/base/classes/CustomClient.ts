64539ca4f3c706675637fee7ee1eac00e5488c9c
Isa Kilic <Is4Kilic@hotmail.com>
2025-05-24 16:16:59 +0200

    refactor: no more gammel

--- src/base/classes/CustomClient.ts
+++ src/base/classes/CustomClient.ts
@@ -2,11 +2,16 @@ import { Client, GatewayIntentBits } from "discord.js";
 import ICustomClient from "../interfaces/ICustomClient";
 import IConfig from "../interfaces/IConfig";
 import Handler from "./Handler";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default class CustomClient extends Client implements ICustomClient {
 
     config: IConfig;
     handler: Handler;
+    token: string;
 
     constructor() {
         super({
@@ -16,18 +21,20 @@ export default class CustomClient extends Client implements ICustomClient {
                 GatewayIntentBits.MessageContent,
             ]
         });
-        this.config = require(`${process.cwd()}/data/config.json`)
-        this.handler = new Handler(this);
-        this.token = process.env.DISCORD_TOKEN ? process.env.DISCORD_TOKEN : '';
 
+        // Load config and token
+        this.config = require(`${process.cwd()}/data/config.json`);
+        this.handler = new Handler(this);
+        this.token = process.env.DISCORD_TOKEN ?? '';
     }
+
     Init(): void {
         if (this.token) {
             this.LoadHandlers();
-            this.login(this.config.token)
+            this.login(this.token)
                 .catch((e) => { console.log("Login Error: ", e) });
         } else {
-            throw new Error('DISCORD TOKEN MISSING')
+            throw new Error('DISCORD_TOKEN is missing in environment variables');
         }
     }
 
